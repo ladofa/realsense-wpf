c9b6a0842336bb0c29ec6de7adc9b1072cbce692
ladofa <ladofa9@gmail.com>
2024-02-26 23:47:10 +0900

    Add project files.

---
+++ RealSenseCam/Cam.cs
@@ -0,0 +1,128 @@
+using System;
+using Intel.RealSense;
+using OpenCvSharp;
+
+namespace RealSenseCam
+{
+    public class Cam
+    {
+        public int DEPTH_FRAME_WIDTH = 640;
+        public int DEPTH_FRAME_HEIGHT = 480;
+
+        public int RGB_FRAME_WIDTH = 1280;
+        public int RGB_FRAME_HEIGHT = 720;
+
+        Pipeline pipe;
+        Align align;
+        double depthScale;
+        public void Init()
+        {
+            //https://community.intel.com/t5/Items-with-no-label/Depth-post-processing-in-%D1%81/td-p/645136?profile.language=en
+            //https://github.com/IntelRealSense/librealsense/issues/8226
+
+            var ctx = new Context();
+            var devices = ctx.QueryDevices();
+            var device = devices[0].As<SerializableDevice>();
+
+            //json 설정을 하고 싶으면 아래 주석을 해제
+            //var device = Intel.RealSense.SerializableDevice.FromDevice(devices[0]);
+            //device.JsonConfiguration = System.IO.File.ReadAllText("L515_SHORT.json");
+
+            pipe = new Pipeline();
+            var config = new Config();
+            config.EnableStream(Stream.Depth, DEPTH_FRAME_WIDTH, DEPTH_FRAME_HEIGHT, Format.Z16, 30);
+            config.EnableStream(Stream.Color, RGB_FRAME_WIDTH, RGB_FRAME_HEIGHT, Format.Bgr8, 30);
+            
+            var cfg = pipe.Start(config);
+            depthScale = device.Sensors[0].DepthScale;
+            depthScale *= 10000;//meter to dmm
+
+            var profile = cfg.GetStream<VideoStreamProfile>(Stream.Color);
+            align = new Align(Stream.Color);
+        }
+
+        //depth의 단위는 dmm
+        public void GetFrame(out Mat depth, out Mat bgr)
+        {
+            if (pipe == null)
+            {
+                depth = new Mat();
+                bgr = new Mat();
+            }
+            var frames = pipe.WaitForFrames();
+            //align the frame
+            frames = align.Process<FrameSet>(frames);
+
+            var colorFrame = frames.ColorFrame.DisposeWith(frames);
+            var depthFrame = frames.DepthFrame.DisposeWith(frames);
+
+            //// If we only received new depth frame, 
+            //// but the color did not update, continue
+            //ulong lastFrameNumber = 0;
+            //if (colorFrame.Number == lastFrameNumber) continue;
+            //lastFrameNumber = colorFrame.Number;
+
+            bgr = FrameToMat(colorFrame);
+            depth = DepthFrameToDeciMilli(ref depthFrame);
+        }
+
+        //Function based off of frame_to_mat https://github.com/IntelRealSense/librealsense/blob/master/wrappers/opencv/cv-helpers.hpp and converted to C#
+        private OpenCvSharp.Mat FrameToMat(Intel.RealSense.Frame f)
+        {
+            var vf = f as VideoFrame;
+            int w = vf.Width;
+            int h = vf.Height;
+            Mat m = null;
+            if (vf.Profile.Format == Format.Bgr8)
+            {
+                m = new Mat(h, w, MatType.CV_8UC3, f.Data);
+                return m;
+            }
+            else if (vf.Profile.Format == Format.Rgb8)
+            {
+                m = new Mat(h, w, MatType.CV_8UC3, f.Data);
+                m.CvtColor(ColorConversionCodes.RGB2BGR);
+                return m;
+            }
+            else if (vf.Profile.Format == Format.Z16)
+            {
+                m = new Mat(h, w, MatType.CV_16UC1, f.Data);
+                return m;
+            }
+            else if (vf.Profile.Format == Format.Y8)
+            {
+                m = new Mat(h, w, MatType.CV_8UC1, f.Data);
+                return m;
+            }
+            else if (vf.Profile.Format == Format.Disparity32)
+            {
+                m = new Mat(h, w, MatType.CV_32FC1, f.Data);
+                return m;
+            }
+            else
+            {
+                //MessageBox.Show("Error occurred!");
+                return null;
+            }
+        }
+
+        //Function to convert depth frame to a matrix of doubles with distances in meters. 
+        //Function based off of depth_frame_to_meters https://github.com/IntelRealSense/librealsense/blob/master/wrappers/opencv/cv-helpers.hpp and converted to C#
+        private Mat DepthFrameToDeciMilli(ref DepthFrame df)
+        {
+            Intel.RealSense.Frame f = df as Intel.RealSense.Frame;
+            Mat dm = FrameToMat(f);
+            dm = dm * depthScale;
+            return dm;
+        }
+
+        public Mat DepthToColor(Mat depth)
+        {
+            Mat gray = new Mat(depth.Size(), MatType.CV_8UC1);
+            Mat vis = new Mat(depth.Size(), MatType.CV_8UC3);
+            Cv2.ConvertScaleAbs(depth, gray, 0.8 / 10000.0 * 255.0); // maxDist / dmm * 255
+            OpenCvSharp.Cv2.ApplyColorMap(gray, vis, ColormapTypes.Jet);
+            return vis;
+        }
+    }
+}
---
+++ RealSenseWpf/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RealSenseCam;
+using cv = OpenCvSharp;
+
+namespace RealSenseWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Cam cam = new Cam();
+
+        
+
+        double getDist(cv.Mat depth, int x, int y)
+        {
+            
+            int margin = 10;
+            // look 20x20 (margin * 2) area
+            var top = Math.Clamp(y - margin, 0, depth.Rows - 1);
+            var bottom = Math.Clamp(y + margin, 0, depth.Rows - 1);
+            var left = Math.Clamp(x - margin, 0, depth.Cols - 1);
+            var right = Math.Clamp(x + margin, 0, depth.Cols - 1);
+            if (top == bottom || left == right)
+            {
+                return 0;
+            }
+
+            var crop = depth[top..bottom, left..right];
+            var m = crop.Mean();
+            return m.Val0;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cam.Init();
+
+            Task.Run(() =>
+            {
+                while (loop)
+                {
+                    cam.GetFrame(out cv.Mat depth, out cv.Mat bgr);
+                    this.depth = depth;
+                    cv.Mat depth_vis = cam.DepthToColor(depth);
+
+                    string output = $"{dist}";
+
+
+
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        ColorImage.Source = cv.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(bgr);
+                        DepthImage.Source = cv.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(depth_vis);
+                        if (dist!= 0) InfoTextBlock.Text = output;
+                    });
+                }
+            });
+        }
+
+        double dist = 0;
+        cv.Mat depth;
+
+        private void ColorImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image image = (Image)sender;
+            var p = e.GetPosition(image);
+            dist = getDist(depth, (int)(p.X / image.ActualWidth * depth.Width), (int)(p.Y / image.ActualHeight * depth.Height));
+        }
+
+        bool loop = true;
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            loop = false;
+        }
+    }
+}
